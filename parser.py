d6a79c33e4838b78dfd8185eeb9993426a8a68d2
Simon Ibssa <ibssasimon@gmail.com>
2019-10-19 19:19:26 -0700

    all my hard work that is going to waste

--- parser.py
+++ parser.py
@@ -1,4 +1,47 @@
 import requests
+import string
 from bs4 import BeautifulSoup as bs
 
-r = requests.get("https://genius.com/Kendrick-lamar-poetic-justice-lyrics")
+def main():
+
+    artist = input("Artist name:  ")
+    song_title = input("Song name: ")
+    html = get_html("https://genius.com/" + construct_song_URL(artist, song_title))
+
+    soup = bs(html.text, 'html.parser')
+
+    lyrics = soup.find(class_="lyrics")
+    p_lyrics = lyrics.find('p')
+    print(p_lyrics)
+# Function to get HTML 
+def get_html(URL):
+    r = requests.get(URL)
+    return r
+
+def construct_song_URL(artist, song_title):
+
+    #Replacing punctuation
+    for c in string.punctuation:
+        song_title = song_title.replace(c, "")
+
+    artist_URL = ""
+    song_URL = ""
+
+
+    artist = artist.lower().capitalize()
+    song_title = song_title.lower()    
+    artist_list = artist.split(" ")
+
+
+    song_title_list = song_title.split(" ")
+
+    for i in artist_list:
+        artist_URL += i + "-"
+
+    for j in range(0, len(song_title_list)):
+        song_URL += song_title_list[j] + "-"
+    return(artist_URL + song_URL + "lyrics")
+
+
+if __name__ == "__main__":
+    main()
